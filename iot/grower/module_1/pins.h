e0c7da0527b8dcb5c1119ba10208dd045041496e
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-02-03 09:36:52 +0700

    assigned pins to module_1

---
+++ iot/grower/module_1/pins.h
@@ -0,0 +1,21 @@
+#ifndef PIN_H
+
+// relays
+#define PIN_LED 16
+#define PIN_FAN 17
+#define PIN_PUMP 18
+#define PIN_SV_WATER 19
+#define PIN_SV_RESERVOIR 21
+
+// sensors
+#define PIN_DHT11_1 22
+#define PIN_DHT11_2 23
+#define PIN_DHT22_1 25
+#define PIN_DHT22_2 26
+#define PIN_US_TRIG 27
+#define PIN_US_ECHO 32
+#define PIN_TDS 35
+#define PIN_PH 36
+#define PIN_SOLNTEMP 33
+
+#endif  /* PIN_H */
